1f9c1872da66857923e84f337a27968a45de00dd
dbetebenner <dbetebenner@gmail.com>
2017-05-26 13:15:32 -0400

    Bumping version number and fixing man page cross-reference

--- R/zzz.R
+++ R/zzz.R
@@ -1,6 +1,6 @@
 `.onAttach` <-
 	function(libname, pkgname) {
 		if (interactive()) {
-			packageStartupMessage(magenta$bold('Literasee',paste(paste0(unlist(strsplit(as.character(packageVersion("Literasee")), "[.]")), c(".", "-", ".", "")), collapse=""),' (5-22-2017). For help: >help("Literasee") or visit https://centerforassessment.github.io/Literasee'))
+			packageStartupMessage(magenta$bold('Literasee',paste(paste0(unlist(strsplit(as.character(packageVersion("Literasee")), "[.]")), c(".", "-", ".", "")), collapse=""),' (5-26-2017). For help: >help("Literasee") or visit https://centerforassessment.github.io/Literasee'))
 	}
 }
