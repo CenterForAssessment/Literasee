effb5194588b550306557a2dbc222f1573976a95
Adam VanIwaarden <avaniwaarden@gmail.com>
2017-06-09 06:21:25 -0600

    Add pageBreak function and change file name to utils for misc functions

--- R/utils.R
+++ R/utils.R
@@ -21,3 +21,23 @@ setCounters <- function(tbl.counter=0, fig.counter=0, eqn.counter=0) {
   options("fig_caption_no" = fig.counter)
   options("equation_counter" = eqn.counter)
 }
+
+getCounter <- function(type = c("table", "figure", "equation")) {
+	if (tolower(type) == "table") return(getOption("table_number"))
+	if (tolower(type) == "figure") return(getOption("fig_caption_no"))
+	if (tolower(type) == "equation") return(getOption("equation_counter"))
+}
+
+
+pageBreak <- function() {
+	###  NOTE:  ALL 'cat()' text must be flush left in R script!	
+	cat("
+<!-- HTML_Start -->
+<div class='breakboth' ></div>
+<!-- LaTeX_Start 
+\\pagebreak
+LaTeX_End -->\n"
+	)
+}
+
+trimWhiteSpace <- function(line) gsub("(^ +)|( +$)", "", line)
