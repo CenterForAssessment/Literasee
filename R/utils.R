1bed6b49522774931f050e3090f90cf2006b40ad
Adam VanIwaarden <avaniwaarden@gmail.com>
2018-11-08 16:26:59 -0700

    Add read.markdown.table

--- R/utils.R
+++ R/utils.R
@@ -34,3 +34,19 @@ LaTeX_End -->\n"
 }
 
 trimWhiteSpace <- function(line) gsub("(^ +)|( +$)", "", line)
+
+read.markdown.table <- function(file, stringsAsFactors = FALSE, strip.white = TRUE, ...){
+    if (length(file) > 1) {
+        lines <- file
+    } else if (grepl('\n', file)) {
+        con <- textConnection(file)
+        lines <- readLines(con)
+        close(con)
+    } else {
+        lines <- readLines(file)
+    }
+    lines <- lines[!grepl('^[[:blank:]+-=:_|]*$', lines)]
+    lines <- gsub('(^\\s*?\\|)|(\\|\\s*?$)', '', lines)
+    read.delim(text = paste(lines, collapse = '\n'), sep = '|',
+               stringsAsFactors = stringsAsFactors, strip.white = strip.white, ...)
+}
