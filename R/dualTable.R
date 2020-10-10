f330c67076e91ebca1a81e26e7c36bada641b9d6
Adam VanIwaarden <avaniwaarden@gmail.com>
2020-10-09 21:46:27 -0600

    Add ability to output dualTable to multiple files (and/or stdout)

--- R/dualTable.R
+++ R/dualTable.R
@@ -7,6 +7,7 @@
   caption="",
   css.class="gmisc_table breakboth",
   use.xtable=FALSE,
+  files="",
   ...) {
   tmp_caption <- tblCap(caption)
   if (all(nchar(align) > 1) & length(align)==1){
@@ -16,13 +17,27 @@
   	tex.align <- align
   	html.align<- paste(align,collapse='')
   }
-  cat("\n<!-- HTML_Start -->\n")
-  html.text <- suppressWarnings(cat(htmlTable::htmlTable(my_tbl, title="", align=html.align, caption=tmp_caption, css.class=css.class,...)))
-  cat("\n<!-- LaTeX_Start\n")
+
+  ###  Utility function - cat to files, including stdout, if requested (for RMD output format)
+  ccat <- function(text, all.files, append = FALSE) {for(f in all.files) cat(text, file=f, append=append)}
+
+  ###  Output HTML and Latex formatted tables
+  ccat("\n<!-- HTML_Start -->\n<!-- dualTable -->\n", all.files=files)
+  html.text <- htmlTable::htmlTable(my_tbl, title="", align=html.align, caption=tmp_caption, css.class=css.class, ...)
+  suppressWarnings(ccat(html.text, all.files=files, append = TRUE))
+  ccat("\n<!-- LaTeX_Start dualTable\n", all.files=files, append = TRUE)
   if (use.xtable) {
-    latex.text <- xtable::print.xtable(xtable::xtable(my_tbl, align=c("r", tex.align), caption=tmp_caption))
+    latex.text <- xtable::print.xtable(xtable::xtable(my_tbl, align=c("r", tex.align), caption=tmp_caption),
+                                       table.placement = where, caption.placement = "top", file=f, append = TRUE)
   } else {
-    latex.text <- print(Hmisc::latex(my_tbl, file="", where=where, col.just=tex.align, caption=tmp_caption, ...))
+    options(omitlatexcom=TRUE)
+    for (f in files) {
+      if (f == "") {
+        latex.text <- print(Hmisc::latex(my_tbl, file=f, append = TRUE, where=where, col.just=tex.align, caption=tmp_caption, ...))
+      } else {
+        latex.text <- Hmisc::latex(my_tbl, file=f, append = TRUE, where=where, col.just=tex.align, caption=tmp_caption, ...)
+      }
+    }
   }
-  cat("\nLaTeX_End -->\n")
+  ccat("\nLaTeX_End -->\n", all.files=files, append = TRUE)
 }
